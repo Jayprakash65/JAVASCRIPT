b8dc2b35d9757aefe6715c21a194db7e5afe5b2b
Jayprakash <163001171+Jayprakash65@users.noreply.github.com>
2024-04-06 11:30:20 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,14 @@
+//----------------------DATES----------------------------
+
+let myDate = new Date();
+//console.log(myDate.toString());
+//console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+console.log(typeof myDate);         // typeof ---> object
+
+let another 
+
+
+
