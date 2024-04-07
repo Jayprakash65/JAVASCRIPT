902dc56a3a89165b40a0dcc7e3788f91f214d72d
Jayprakash <163001171+Jayprakash65@users.noreply.github.com>
2024-04-07 09:03:40 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,7 +1,45 @@
-//ARRAY
+              //ARRAY
 
 //HOW to declare array
 
-const meraarr = [1, 2, 3, 4, 5]
+    const myarr = [0, 1, 2, 3, 4, 5]
+    let fruits = ['apple', 'orange', 'banana']
+    const myarr2 = new Array(1, 2, 3, 4, 5, 6)
 
-console.log(meraarr[4]);
+    //console.log(myarr2[5]);
+
+
+//// Array Methods
+
+    // myarr.push(10)     // Ye method array mein ek ya zyada items ko array ke end mein add karne ke liye use hota hai. 
+    // myarr.push(11)
+    // myarr.pop()        // Ye method array se last item ko hata deta hai.
+
+    //myarr.unshift(9)      // Ye method array ke shuruwat mein ek ya zyada items ko add karne ke liye use hota hai. 
+    //myarr.shift()         // Ye method array se first item ko hata deta hai.
+    
+    
+ // .join() method ka istemal array ke saare elements ko ek string mein jodne ke liye
+
+    let myarrStr = myarr.join()
+
+    // console.log(meraarr);     // Default separator ke saath
+    // console.log(meraarrStr);  // Output: "apple,banana,orange,grapes" 
+    
+    
+// slice, splice
+
+    console.log("A:" , myarr);
+
+    const mya1 = myarr.slice(1, 3)
+    console.log(mya1);
+
+    console.log("B:" , myarr);
+
+    const mya2 = myarr.splice(1, 3)
+    console.log(mya2);
+
+
+
+
+
