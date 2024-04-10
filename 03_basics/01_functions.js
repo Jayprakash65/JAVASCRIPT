985de1522dc2cd5e3bbcc46be05f3f36d6e75371
Jayprakash <163001171+Jayprakash65@users.noreply.github.com>
2024-04-10 11:34:01 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+function addNumber(num1, num2) {
+    
+    return num1 + num2
+}
+
+let result = addNumber (5, 10)
+// console.log("Result:", result);
+
+
+
+
+function loginUserMessage(username) {
+    
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("jayprakash"));
+
+
+/* In case , agar aap loginUserMessage function ko bina kisi argument ke call karte hain, toh username parameter undefined hoga */
+
+ console.log(loginUserMessage());    // Output: "undefined just logged in"
+
+
+
+
+ function loginUserMessage(username) {
+    if (username === undefined) {
+        return "Plz enter your name";
+    } else {
+        return `${username} just logged in`;
+    }
+}
+
+console.log(loginUserMessage("jayprakash")); // Output: "jayprakash just logged in"
+console.log(loginUserMessage()); // Output: "Plz enter your name"
