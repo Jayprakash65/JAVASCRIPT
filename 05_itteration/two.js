5092ded6360ebc52346a787262f0dfea257ec686
Jayprakash <163001171+Jayprakash65@users.noreply.github.com>
2024-04-20 10:03:45 +0000

    two

---
+++ 05_itteration/two.js
@@ -0,0 +1,26 @@
+// while loop 
+
+let i = 0
+while (i <= 15) {
+    //console.log(`value of i is ${i}`);
+    i = i + 2
+}
+
+
+let myArray = ['falsh', 'bat', 'super']
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// do- while loop --> pahle kam fir condition check hogi
+
+let score = 1    // samjo score ka value 11 ho jaye tho bhi output a jayega score is 11 keki kamaple fir condition check ho raha hai 
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
